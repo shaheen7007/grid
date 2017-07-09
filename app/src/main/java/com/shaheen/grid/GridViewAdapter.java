451437e35111f2ced5e28f2bd141526403afcab4
shaheen-mac <shaheenbsv@gmail.com>
2017-07-09 13:51:31 +0530

    Initial commit

---
+++ app/src/main/java/com/shaheen/grid/GridViewAdapter.java
@@ -0,0 +1,83 @@
+package com.shaheen.grid;
+
+/**
+ * Created by shaheen-mac on 09/07/17.
+ */
+        import android.content.Context;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.BaseAdapter;
+        import android.widget.GridView;
+        import android.widget.ImageView;
+        import android.widget.LinearLayout;
+        import android.widget.TextView;
+
+        import com.android.volley.toolbox.ImageLoader;
+        import com.android.volley.toolbox.NetworkImageView;
+
+        import java.util.ArrayList;
+
+
+public class GridViewAdapter extends BaseAdapter {
+
+    //Imageloader to load images
+    private ImageLoader imageLoader;
+
+    //Context
+    private Context context;
+
+    //Array List that would contain the urls and the titles for the images
+    private ArrayList<String> images;
+    private ArrayList<String> names;
+
+    public GridViewAdapter (Context context, ArrayList<String> images, ArrayList<String> names){
+        //Getting all the values
+        this.context = context;
+        this.images = images;
+        this.names = names;
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return images.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //Creating a linear layout
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View grid;
+        if (convertView == null) {
+
+            grid = new View(context);
+            grid = inflater.inflate(R.layout.grid_single, null);
+            TextView textView = (TextView) grid.findViewById(R.id.grid_text);
+            NetworkImageView imageView = (NetworkImageView) grid.findViewById(R.id.grid_image);
+            imageLoader = CustomVolleyRequest.getInstance(context).getImageLoader();
+            imageLoader.get(images.get(position), ImageLoader.getImageListener(imageView, R.mipmap.ic_launcher, android.R.drawable.ic_dialog_alert));
+            imageView.setImageUrl(images.get(position),imageLoader);
+            textView.setText(names.get(position));
+        } else {
+            grid = (View) convertView;
+        }
+
+        return grid;
+    }
+}
+
+
+
+
